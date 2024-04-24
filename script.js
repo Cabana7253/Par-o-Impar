7ea9867441622903d3e8fe1036d2bca6c829af28
Nathaly Cabana <161664415+Cabana7253@users.noreply.github.com>
2024-04-24 15:39:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Vamos a jugar es Par o Impar")
+
+let n;
+n = parseInt(prompt("Ingresa un Numero"));
+
+if(n % 2 == 0){
+    alert("El numero "+n+" es par");
+}else{
+    alert("El numero "+n+" es imapar");
+}
